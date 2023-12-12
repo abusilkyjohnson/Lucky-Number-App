547267f6c25eac9dedcf7e55dedd37b9f088f7e4
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-11 22:15:21 -0500

    random method and textview is set to the random method

--- app/src/main/java/com/example/luckynumbergenerator/YourNumberPage.java
+++ app/src/main/java/com/example/luckynumbergenerator/YourNumberPage.java
@@ -3,6 +3,10 @@ package com.example.luckynumbergenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class YourNumberPage extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class YourNumberPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_your_number_page);
+
+        TextView randomText = findViewById(R.id.LuckyNum);
+
+        randomText.setText("" + RandomNum());
     }
+    public int RandomNum()
+    {
+        Random rand = new Random();
+        int randomInt = rand.nextInt(100);
+        return randomInt;
+    }
+
 }
