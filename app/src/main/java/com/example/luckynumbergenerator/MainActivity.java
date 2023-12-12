82682a57ec5cb948e110e387af90a36b8c96f26e
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-11 22:59:35 -0500

    all functionality working and being shared also assigned random method variable

--- app/src/main/java/com/example/luckynumbergenerator/MainActivity.java
+++ app/src/main/java/com/example/luckynumbergenerator/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        EditText userNameXML = findViewById(R.id.EnterName);
         Button luckyButt = findViewById(R.id.randogenerator);
 
         luckyButt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String userName = userNameXML.getText().toString();
+
+                // Explicit Intent
+                Intent i = new Intent(getApplicationContext(), YourNumberPage.class);
+
+                //passing the name to the secondpage/activity the put extra can send many different data value types
+                i.putExtra("name", userName);
+
+                startActivity(i);//always need this when using intents
                 GoToLucky();
             }
         });
--- app/src/main/java/com/example/luckynumbergenerator/YourNumberPage.java
+++ app/src/main/java/com/example/luckynumbergenerator/YourNumberPage.java
@@ -2,8 +2,11 @@ package com.example.luckynumbergenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -17,13 +20,48 @@ public class YourNumberPage extends AppCompatActivity {
 
         TextView randomText = findViewById(R.id.LuckyNum);
 
-        randomText.setText("" + RandomNum());
+        int random_num  = RandomNum();
+
+        Intent i = getIntent();//allows us to get intent from key we did in the put extra method
+        String userName = i.getStringExtra("name");
+
+        if(userName.length() == 0)
+        {
+            randomText.setText("Please put your name first");
+
+        }
+        else
+        {
+            randomText.setText("" + random_num);
+        }
+
+
+        Button shareButt = findViewById(R.id.shareButt);
+
+        shareButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            SharedData(userName,random_num);
+            }
+        });
     }
     public int RandomNum()
     {
         Random rand = new Random();
         int randomInt = rand.nextInt(100);
         return randomInt;
+    }
+
+    public void SharedData(String userName, int randomNum)
+    {
+        //Implicit Intent
+        Intent i = new Intent(Intent.ACTION_SEND);//action send i s used to send data
+        i.setType("text/plain");
+
+        i.putExtra(Intent.EXTRA_SUBJECT, userName + "Got Lucky Number " );
+        i.putExtra(Intent.EXTRA_TEXT, "Lucky Num is " + randomNum);
+
+        startActivity(Intent.createChooser(i, "Choose A platform"));
     }
 
 }
