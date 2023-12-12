fdfebef3c44b89a97e6b7c610501b43b1cb34dab
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-12-11 21:11:12 -0500

    second page made n generate button goes the

--- app/src/main/java/com/example/luckynumbergenerator/MainActivity.java
+++ app/src/main/java/com/example/luckynumbergenerator/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.luckynumbergenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button luckyButt = findViewById(R.id.randogenerator);
+
+        luckyButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToLucky();
+            }
+        });
     }
+
+    public void GoToLucky(){
+
+        Intent intent = new Intent(this, YourNumberPage.class);
+        //1st parameter = current location //2nd = destination
+        startActivity(intent);
+    }
+
 }
+
